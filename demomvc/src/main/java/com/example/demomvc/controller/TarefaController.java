2a115adc7ab762c48d4c9ba1744a1d403dd9ce78
dougl <dougl@Douglas>
2025-08-08 10:03:15 -0300

    Persistencias de Dados e acesso ao Banco de Dados

--- demomvc/src/main/java/com/example/demomvc/controller/TarefaController.java
+++ demomvc/src/main/java/com/example/demomvc/controller/TarefaController.java
@@ -3,20 +3,26 @@ package com.example.demomvc.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demomvc.entity.Tarefa;
+import com.example.demomvc.service.TarefaService;
 
 
 @Controller
 @RequestMapping("/tarefas")
 public class TarefaController {
 	
-	List<Tarefa> tarefas = new ArrayList<>();
+	@Autowired
+	private TarefaService service;
+	
 	@GetMapping("/cadastro")
 	public String cadastro(Tarefa tarefa) {
 		return "tarefas/cadastro";
@@ -24,27 +30,40 @@ public class TarefaController {
 	
 	@PostMapping("/salvar")
 	public String salvar(Tarefa tarefa) {
+		service.salvar(tarefa);
 		
-		Long id = tarefas.size() + 1L;
-		Tarefa t = new Tarefa();
-		
-		t.setId(id);
-		t.setNome("tarefa");
-		t.setDataEntrega(tarefa.getDataEntrega());
-		t.setResponsavel(tarefa.getResponsavel());
-		
-		tarefas.add(t);
-		
-		return "redirect:/tarefas/list";
+		return "redirect:/tarefas/lista";
 		
 	}
 	
 	@GetMapping("/lista")
-	public ModelAndView list() {
-		ModelAndView mv = new ModelAndView("tarefas/lista");
-		mv.addObject("tarefas", tarefas);
+	public String lista(ModelMap model) {
+		model.addAttribute("tarefas", service.buscaTodos());
+		return "tarefas/lista";
 		
-		return mv;
 	}
+	
+	// editar e excluir
+
+			@PostMapping("/editar")
+			public String editar(Tarefa tarefa) {
+				service.editar(tarefa);
+				return "redirect:/tarefas/cadastro";
+				
+			}
+
+			@GetMapping("/excluir/{id}")
+			public String excluir(@PathVariable("id") Long id, ModelMap model) {
+				
+				service.excluir(id);
+				return lista(model);
+			}
+
+			@GetMapping("/editar/{id}")
+			public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+				model.addAttribute("tarefa", service.buscarPorId(id));
+				
+				return "tarefas/cadastro";
+			}
 
 }
